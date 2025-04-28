8c7fe70de06e5196774d9e67647d12c98f4e4a41
danielpsrb <danielpsrb01@gmail.com>
2025-04-28 16:54:50 +0800

    update model user

--- auth_app/models.py
+++ auth_app/models.py
@@ -15,13 +15,13 @@ class UserManager(BaseUserManager):
     
     def create_superuser(self, email, first_name, last_name, username, password=None):
         user = self.create_user(email, first_name, last_name, username, password)
-        user.is_admin = True
+        user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
         return user
 
 
-class CustomUser(AbstractBaseUser, PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(
         primary_key=True,
         unique=True,
