e3f4ecca6e31d1f627c39886932d86380ce95dd1
danielpsrb <danielpsrb01@gmail.com>
2025-06-09 12:31:16 +0800

    graphql api schema update

--- gql_api/schema.py
+++ gql_api/schema.py
@@ -4,10 +4,39 @@ import graphql_jwt
 from auth_app.graphql.mutations import AuthMutations
 from auth_app.graphql.queries import Query as AuthQueries
 
-class Query(AuthQueries, graphene.ObjectType):
+from charivol.graphql.mutations import (
+    DonorMutations,
+    VolunteerMutations,
+    DonationAreaMutations,
+    DonationMutations
+)
+
+from charivol.graphql.queries import (
+    DonorQuery,
+    VolunteerQuery,
+    DonationAreaQuery,
+    DonationQuery
+)
+
+class Query(
+    AuthQueries,
+    DonorQuery,
+    VolunteerQuery,
+    DonationAreaQuery,
+    DonationQuery,
+    graphene.ObjectType
+):
+    # Additional queries if needed
     pass
 
-class Mutation(AuthMutations, graphene.ObjectType):
+class Mutation(
+    AuthMutations,
+    DonorMutations,
+    VolunteerMutations,
+    DonationAreaMutations,
+    DonationMutations,
+    graphene.ObjectType
+):
     # JWT Auth mutations
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
