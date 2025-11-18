aea80c0e2c323188dc6d8bff6d10023ecd356258
danielpsrb <danielpsrb01@gmail.com>
2025-11-18 11:20:31 +0800

    update config

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -56,5 +56,5 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-    path('', lambda request: JsonResponse({"message": "Hi There, this REST API created on Django! "}), name='home'),
+    path('', lambda request: JsonResponse({"message": "Hi There, this API created on Django version 5.2 "}), name='home'),
 ]
